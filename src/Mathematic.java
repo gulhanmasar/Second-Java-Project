620ff31a68c08a252c8dd1908a2e3f4221b8e001
gulhanmasar <109763090+gulhanmasar@users.noreply.github.com>
2022-07-24 19:15:07 +0300

    Numbers Working

---
+++ src/Mathematic.java
@@ -0,0 +1,13 @@
+public class Mathematic {
+    public static void main(String[] args){
+        int taban =3 , kuvvet =4;
+        long sonuc =1;
+        for(;kuvvet !=0;--kuvvet)
+        {
+            sonuc *=taban;
+        }
+        System.out.println("Cevap=" + sonuc);
+
+    }
+
+}
---
+++ src/Numbers2.java
@@ -0,0 +1,12 @@
+public class Numbers2 {
+
+    public static void main(String[] args){
+        int i1 = 100;
+        int i2= 200;
+        int toplam = (i1 + i2);
+
+        System.out.println("Sayıların Toplamı" + toplam);
+
+
+    }
+}
