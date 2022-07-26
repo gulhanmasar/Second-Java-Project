768e025804fdb06b27f9ce5e6ecde992a6c29657
gulhanmasar <109763090+gulhanmasar@users.noreply.github.com>
2022-07-26 23:00:52 +0300

    CarClass created

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+     String brand = "porshe";
+     String model ="cayen";
+     int year = 2019;
+     int maxAccelerate;
+     public int getMinAcc (int maxAccelerate){
+         return maxAccelerate -160;
+     }
+
+}
+
---
+++ src/CarModel.java
@@ -0,0 +1,14 @@
+public class CarModel {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        System.out.println(car.model);
+        System.out.println(car.brand);
+        System.out.println(car.maxAccelerate);
+        System.out.println(car.getMinAcc(300));
+
+
+
+    }
+}
