857ea7d1ffa0b55b72b057b102f5b524c8598780
gulhanmasar <109763090+gulhanmasar@users.noreply.github.com>
2022-07-28 17:54:17 +0300

    Naming class created

---
+++ src/Naming.java
@@ -0,0 +1,26 @@
+public class Naming {
+    private int anIntegerVariable;
+    private double aDoubleVariable;
+     public static void main(String[] args) {
+        Naming namingDemo = new Naming();
+     namingDemo.anIntegerVariable = 95;
+     namingDemo.aDoubleVariable =0.35;
+     namingDemo.aSimpleMethod(2,5);
+     namingDemo.anotherSimpleMethod(8,4,false);
+     
+     }
+
+    private void anotherSimpleMethod(int i, int i1, boolean b) {
+        System.out.println("in anotherSimpleMethod ( int aParameter, long anotherParameter,false)" );
+    }
+
+
+    public void aSimpleMethod(int i, int i1) {
+         System.out.println("in aSimpleMethod(int aParameter , int anotherParameter");
+         
+
+
+                    
+
+    }
+}
