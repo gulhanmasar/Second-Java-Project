fa6071aa020cfa507d5f30a6d40f57d159b03952
gulhanmasar <109763090+gulhanmasar@users.noreply.github.com>
2022-07-31 13:07:41 +0300

    Naming and if else statements

--- src/Naming.java
+++ src/Naming.java
@@ -1,17 +1,31 @@
 public class Naming {
-    private int anIntegerVariable;
-    private double aDoubleVariable;
+public int anIntegerVariable = 5;
+   public double aDoubleVariable;
+
+   public  boolean isInteger = false;
      public static void main(String[] args) {
-        Naming namingDemo = new Naming();
+         Naming namingDemo = new Naming();
      namingDemo.anIntegerVariable = 95;
      namingDemo.aDoubleVariable =0.35;
+
      namingDemo.aSimpleMethod(2,5);
      namingDemo.anotherSimpleMethod(8,4,false);
+
      }
 
     public void aSimpleMethod(int aParameter, int anotherParameter){
-        System.out.println("in aSimpleMethod(int aParameter, int anotherParameter)");
-    }
+        isInteger = true;
+        System.out.println("in aSimpleMethod" +  aParameter+ "ve" + anotherParameter);
+        if(isInteger == true){
+            System.out.println(anIntegerVariable +5);
+anIntegerVariable ++;
+            System.out.println(anIntegerVariable +5);
+        }
+        else{
+           System.out.println("bu bir false ifadedir");
+
+        }
+        }
 
     public void anotherSimpleMethod(int aParameter, long anotherParameter, boolean yetAnotherParameter){
         System.out.println("in anotherSimpleMethod(int aParameter, long anotherParameter, boolean yetAnotherParameter)");
