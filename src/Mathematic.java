399c2d395079242bbf8b536090bb134f017b9338
gulhan <msrglhn@gmail.com>
2022-08-05 00:02:17 +0300

    Working with Constructor in class

--- src/Mathematic.java
+++ src/Mathematic.java
@@ -1,5 +1,22 @@
 public class Mathematic {
+
+
+    int number;
+    String name;
+    public Mathematic(int num, String ad){
+
+        this.name = ad;
+        this.number = num;
+
+    }
+
+
     public static void main(String[] args){
+
+        Mathematic ma = new Mathematic(1, "gulhan");
+
+        System.out.println("My name is " + ma.name);
+
         int taban =3 , kuvvet =4;
         long sonuc =1;
         for(;kuvvet !=0;--kuvvet)
@@ -15,6 +32,10 @@ public class Mathematic {
               toplam += i;
           }
 System.out.println("1-1oo arasındaki sayıların toplamı =" + toplam);
+
+
     }
+
+
 
 }
