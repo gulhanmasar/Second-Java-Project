c57dd7cb67f8234b0ba670e395846d54321f65ed
gulhan <msrglhn@gmail.com>
2022-08-18 22:47:27 +0300

    ListCharactersWithIf class created.

---
+++ src/ListCharactersWithIf.java
@@ -0,0 +1,13 @@
+public class ListCharactersWithIf {
+
+
+        public static void main(String[] args) {
+
+            for (char c = 0; c < 65535; c++) {
+                System.out.print("\t" + (int) c + " : " + c);
+                if (c % 10 == 0 & c != 0 & c != 10)
+                    System.out.print("\n");
+            }
+        }
+    }
+
