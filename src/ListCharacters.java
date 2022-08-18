ae9e678516eb3b8ba3c4f6205df004728040517a
gulhan <msrglhn@gmail.com>
2022-08-18 22:44:46 +0300

    ListCharacters class created.

---
+++ src/ListCharacters.java
@@ -0,0 +1,10 @@
+public class ListCharacters {
+
+        public static void main(String[] args) {
+
+            for(char c = 0; c < 65535; c++)
+                System.out.println("value: " + (int)c + " character: " + c);
+        }
+    }
+
+
